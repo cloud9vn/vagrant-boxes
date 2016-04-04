9c02db81886f8c7d5b8a829814b1cd53159c7691
Vuong Minh Nguyen <vuong.nm@cloud9.local>
2016-04-04 18:56:40 +0700

    Update from Apache 2.4 to Apache 2.2

--- centOS6.6x64/php5.6/README.md
+++ centOS6.6x64/php5.6/README.md
@@ -2,4 +2,4 @@
 A box which is pre-installed:
 - PHP 5.6.20
 - Postgresql 9.4.7
-- Apache 2.4.6
+- Apache 2.2.15
--- centOS6.6x64/php5.6/definitions/base.sh
+++ centOS6.6x64/php5.6/definitions/base.sh
@@ -6,13 +6,9 @@ EOM
 
 # Install REMI Repository 
 yum install -y http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-#install Apache 2.4
-wget https://repos.fedorapeople.org/repos/jkaluza/httpd24/epel-httpd24.repo -O /etc/yum.repos.d/httpd24.repo
-yum --enablerepo=remi -y install httpd24
-chkconfig httpd24-httpd on
-
-#install php5.6
+#install php5.6 and Apache 2.2
 yum install -y php php56-php-mbstring php56-php-pdo php-pgsql --enablerepo=remi,remi-php56
+chkconfig httpd on
 # Install Postgresql
 rpm -Uvh http://yum.postgresql.org/9.4/redhat/rhel-6-x86_64/pgdg-centos94-9.4-1.noarch.rpm
 yum install -y postgresql94-server postgresql94-contrib
